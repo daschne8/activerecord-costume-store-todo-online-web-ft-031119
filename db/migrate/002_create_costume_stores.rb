c9cd583e4a8dd2c3a8a480a94910747e22d9cde4
Daniel Schneider <daschne8@gmail.com>
2019-04-16 18:47:18 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,7 +1,13 @@
-class CreateCostumeStores < ActiveRecord::Migration
+class CreateCostumeStores < ActiveRecord::Migration[4.2]
   def change
     create_table :costume_stores do |cs|
-
+      cs.string :name
+      cs.string :location
+      cs.string :inventory
+      cs.integer :emplyees_count
+      cs.boolean :open
+      cs.datetime :opening_time
+      cs.datetime :closing_time
     end
   end
 end
