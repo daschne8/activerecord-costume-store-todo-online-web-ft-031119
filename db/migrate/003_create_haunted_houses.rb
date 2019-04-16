a001ff08f5e5a32085a3526fdfe13350c66f42b8
Daniel Schneider <daschne8@gmail.com>
2019-04-16 18:58:26 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,6 +1,10 @@
 class CreateHauntedHouses < ActiveRecord::Migration[4.2]
   def change
     create_table :haunted_houses do |h|
+      h.string :name
+      h.string :location
+      h.string :theme
+      h.integer :price
     end
   end
 end
