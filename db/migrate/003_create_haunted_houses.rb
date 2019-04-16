552e5072e83654d863ec5e76daf19c081711851e
Daniel Schneider <daschne8@gmail.com>
2019-04-16 19:00:40 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -6,6 +6,7 @@ class CreateHauntedHouses < ActiveRecord::Migration[4.2]
       h.string :theme
       h.integer :price
       h.boolean :family_friendly
+      h.datetime :opening_date
       h.datetime :closing_date
       h.string :description
     end
