61cf3aa988ae2193dad498cefc6545232fb80189
Daniel Schneider <daschne8@gmail.com>
2019-04-16 18:53:00 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -4,8 +4,8 @@ class CreateCostumeStores < ActiveRecord::Migration[4.2]
       cs.string :name
       cs.string :location
       cs.integer :costume_inventory
-      cs.integer :num_of_emplyees
-      cs.boolean :still_in_buisiness
+      cs.integer :num_of_employees
+      cs.boolean :still_in_business
       cs.datetime :opening_time
       cs.datetime :closing_time
     end
